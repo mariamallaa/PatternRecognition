efae83687d6dc9a3eb68a4d5d7aa9702240e14e0
Maram Khattab <maram.khattab@hotmail.com>
2019-12-04 15:43:44 +0200

    baseline

--- line segmentation.py
+++ line segmentation.py
@@ -10,35 +10,54 @@ import numpy as np
 from commonfunctions import *
 from preprocessing import *
 import cv2
+from skimage.morphology import thin
 
-#reading the image
-img=io.imread("scanned\capr6.png")
+# reading the image
+img = io.imread("scanned\capr2.png")
 
-#skew correct with bounding rect
-corrected=correct_skew(img)
-#indices of lines
-lines_indices=line_segmentation(corrected)
-#image with lines
-lines_segmented=corrected.copy()
-lines_segmented[lines_indices]=0.5
-#images into words
-# separators=words_segmentation(corrected,lines_indices)
-# separated_words=lines_segmented.copy()
-# separated_words[:,separators]=0.5
+# skew correct with bounding rect
+corrected = correct_skew(img)
+corrected = np.round(corrected)
 
-separators=words_segmentation(corrected,lines_indices)
-print(separators[0][1])
-words=corrected.copy()
-for i in range(len(lines_indices)-1):
-    for j in range(len(separators[i])-1):
-        words= cv2.rectangle(words,(separators[i][j],lines_indices[i]),(separators[i][j+1],lines_indices[i+1]),0.5,1)
+# indices of lines
+lines_indices = line_segmentation(corrected)
+# image with lines
+lines_segmented = corrected.copy()
+lines_segmented[lines_indices] = 0.5
+# images into words
+separators = words_segmentation(corrected, lines_indices)
 
-vertical=corrected.copy()
-vertical[:,separators[0]]=0.5
+words = corrected.copy()
+for i in range(len(lines_indices) - 1):
+    for j in range(len(separators[i]) - 1):
+        words = cv2.rectangle(
+            words,
+            (separators[i][j], lines_indices[i]),
+            (separators[i][j + 1], lines_indices[i + 1]),
+            0.5,
+            1,
+        )
 
-first=corrected[lines_indices[0]:lines_indices[1],separators[0][1]:separators[0][2]]
-#viewing the images
-viewer=CollectionViewer([img, 1-corrected, lines_segmented,vertical,words])
+# word
+first = corrected[
+    lines_indices[0] : lines_indices[1], separators[0][1] : separators[0][2]
+]
+
+# character segmentation for a line
+line = corrected[lines_indices[0] : lines_indices[1], :]
+np.round(line)
+projection = np.sum(line, axis=1)
+baselineIndex = np.argmax(projection)
+line[baselineIndex, :] = 0.5
+verticalChange = []
+for i in range(baselineIndex):
+    verticalChange.append(len(np.where(line[i, :-1] != line[i, 1:])[0]))
+verticalChange = np.asarray(verticalChange)
+maxChangeIndex = np.argmax(verticalChange)
+
+line[maxChangeIndex, :] = 0.3
+
+# viewing the images
+viewer = CollectionViewer([img, 1 - corrected, lines_segmented, words, line])
 viewer.show()
-
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,20 +1,23 @@
 import numpy as np
 from scipy.ndimage import interpolation as inter
 import cv2
+
+
 def find_score(arr, angle):
     data = inter.rotate(arr, angle, reshape=False, order=0)
     hist = np.sum(data, axis=1)
     score = np.sum((hist[1:] - hist[:-1]) ** 2)
     return score
 
-#using for loops we will not use it IGNORE
+
+# using for loops we will not use it IGNORE
 def adjust_tilt(img):
-    bin_img=img.copy()
+    bin_img = img.copy()
     bin_img = 1 - (bin_img / 255.0)
 
     delta = 1
     limit = 5
-    angles = np.arange(-limit, limit+delta, delta)
+    angles = np.arange(-limit, limit + delta, delta)
     scores = []
     for angle in angles:
         score = find_score(bin_img, angle)
@@ -23,12 +26,14 @@ def adjust_tilt(img):
     best_score = max(scores)
     best_angle = angles[scores.index(best_score)]
     corrected_img = inter.rotate(bin_img, best_angle, reshape=False, order=0)
+    #corrected_img= np.round(corrected_img)
 
     return corrected_img
 
+
 def correct_skew(img):
-    thresh=img.copy()
-    thresh=1-(thresh/255)
+    thresh = img.copy()
+    thresh = 1 - (thresh / 255)
 
     coords = np.column_stack(np.where(thresh > 0))
 
@@ -36,66 +41,70 @@ def correct_skew(img):
     if angle < -45:
         angle = -(90 + angle)
     else:
-        angle=-angle
+        angle = -angle
 
-    
     (h, w) = img.shape[:2]
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(img, M, (w, h),flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    rotated=1-(rotated/255)
+    rotated = cv2.warpAffine(
+        img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE
+    )
+    rotated = 1 - (rotated / 255)
     return rotated
 
-def line_segmentation(img):
-    projection=np.sum(img,axis=1)
-    
-    lines_indices=[]
-    i=0
-    start=0
-    end=0
-    while i < len(projection):
-        if projection[i]==0:
-            start=i
-            j=0
 
-            while projection[j+i]==0 and j+i<len(projection):
-                j+=1
-                if i+j==len(projection)-1:
-                    end=i+j
-                    lines_indices.append(int(start+((end-start)/2)))
+def line_segmentation(img):
+    projection = np.sum(img, axis=1)
+
+    lines_indices = []
+    i = 0
+    start = 0
+    end = 0
+    while i < len(projection):
+        if projection[i] == 0:
+            start = i
+            j = 0
+
+            while projection[j + i] == 0 and j + i < len(projection):
+                j += 1
+                if i + j == len(projection) - 1:
+                    end = i + j
+                    lines_indices.append(int(start + ((end - start) / 2)))
                     return lines_indices
-            end=i+j
-            lines_indices.append(int(start+((end-start)/2)))
-            i=i+j
+            end = i + j
+            lines_indices.append(int(start + ((end - start) / 2)))
+            i = i + j
         else:
-            i+=1
+            i += 1
     return lines_indices
+
 
 def segments_indices(projection):
-    lines_indices=[]
-    i=0
-    start=0
-    end=0
+    lines_indices = []
+    i = 0
+    start = 0
+    end = 0
     while i < len(projection):
-        if projection[i]==0:
-            start=i
-            j=0
+        if projection[i] == 0:
+            start = i
+            j = 0
 
-            while projection[j+i]==0 and j+i<len(projection):
-                j+=1
-                if i+j==len(projection)-1:
-                    end=i+j
-                    lines_indices.append(int(start+((end-start)/2)))
+            while projection[j + i] == 0 and j + i < len(projection):
+                j += 1
+                if i + j == len(projection) - 1:
+                    end = i + j
+                    lines_indices.append(int(start + ((end - start) / 2)))
                     return lines_indices
-            end=i+j
-            lines_indices.append(int(start+((end-start)/2)))
-            i=i+j
+            end = i + j
+            lines_indices.append(int(start + ((end - start) / 2)))
+            i = i + j
         else:
-            i+=1
+            i += 1
     return lines_indices
 
+
 def words_segmentation(img, lines):
-    
+
     # line=img[lines[0]:lines[1],:]
     # projection=projection=np.sum(line,axis=0)
     # print(lines[1]-lines[0])
@@ -103,17 +112,14 @@ def words_segmentation(img, lines):
     # projection=np.convolve(projection,subword,'same')
     # indices=segments_indices(projection)
     # return indices
-    words_rects=[]
-    for i in range(len(lines)-1):
-        line=img[lines[i]:lines[i+1],:]
-        projection=np.sum(line,axis=0)
-        projection=np.convolve(projection,np.array([1,1]),'same')
-        indices=segments_indices(projection)
-       
-        words_rects.append(indices)
-    
-    
-    return words_rects
+    words_rects = []
+    for i in range(len(lines) - 1):
+        line = img[lines[i] : lines[i + 1], :]
+        projection = np.sum(line, axis=0)
+        projection = np.convolve(projection, np.array([1, 1]), "same")
+        indices = segments_indices(projection)
 
-        
+        words_rects.append(indices)
+
+    return words_rects
 
