d6c78a924af2deb20a8d1f98cf5d377d81958338
Maram Khattab <maram.khattab@hotmail.com>
2019-12-13 03:44:43 +0200

    initial trial not working yet

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.ndimage import interpolation as inter
+from scipy import stats
 import cv2
 
 
@@ -96,3 +97,106 @@ def words_segmentation(img, lines):
 
     return words_rects
 
+
+def character_segmentation(wordSkeleton, baselineIndex):
+    # finding maximum transition index
+    verticalChange = []
+    for i in range(baselineIndex):
+        verticalChange.append(
+            len(np.where(wordSkeleton[i, :-1] != wordSkeleton[i, 1:])[0])
+        )
+    verticalChange = np.asarray(verticalChange)
+    maxChangeIndex = max(np.argmax(verticalChange), baselineIndex - 3)
+
+    # getting separation region indices
+
+    separationIndices = np.where(
+        wordSkeleton[maxChangeIndex, :-1] != wordSkeleton[maxChangeIndex, 1:]
+    )[0]
+    separationIndices = separationIndices.reshape(-1, 2)
+
+    # getting cut indices
+    vp = np.sum(wordSkeleton, axis=0)
+    mvf = stats.mode(vp[vp != 0]).mode[0]
+    vpAbove = np.sum(wordSkeleton[:baselineIndex, :])
+    vpBelow = np.sum(wordSkeleton[baselineIndex + 1 :, :])
+    cutIndices = []
+    cutIndices.append(0)
+    for i in range(separationIndices.shape[0] - 1):
+        midRegion = separationIndices[i, 1] + int(
+            (separationIndices[i + 1, 0] - separationIndices[i, 1]) / 2
+        )
+        while vp[midRegion] > mvf:
+            midRegion += 1
+
+        # 3ashan lamma el7oroof beyet2esem menha 7etta soghayara keda lwahdaha
+        if np.sum(wordSkeleton[:, cutIndices[-1] : midRegion]) > 3:
+            if midRegion - cutIndices[-1] > 2:
+                cutIndices.append(midRegion)
+
+    #
+    if np.sum(wordSkeleton[:, cutIndices[-1] : wordSkeleton.shape[1]]) > 3:
+        cutIndices.append(wordSkeleton.shape[1] - 1)
+
+    cutIndices = list(dict.fromkeys(cutIndices))
+    # strokes detection
+
+    print(baselineIndex)
+    strokesIndices = []
+    length = len(cutIndices) - 1
+
+    for i in range(length):
+        segment = wordSkeleton[:, cutIndices[i] : cutIndices[i + 1]]
+        sumTopProjection = (np.sum(segment[0:baselineIndex, :], axis=1)).sum()
+        sumBottomProjection = (np.sum(segment[baselineIndex + 1 :, :], axis=1)).sum()
+
+        if sumTopProjection > sumBottomProjection:
+            vp = np.sum(segment[:baselineIndex, :], axis=0)
+            strokesHeight = np.max(vp)
+            print(cutIndices[i], "passed condition1")
+            h = np.sort(np.sum(segment, axis=1))[::-1][0]
+
+            if strokesHeight < h:
+                print(cutIndices[i], "passed condition2 strokes Height=", strokesHeight)
+                hp = np.sum(segment[:baselineIndex, :], axis=1)
+                hp = hp[hp != 0]
+
+                if stats.mode(hp).mode[0] == mvf:
+                    print(cutIndices[i], "passed condition3")
+                    strokesIndices.append(i)
+            elif len(strokesIndices) >= 2:
+                if i - strokesIndices[-1] == 1 and i - strokesIndices[-2] == 2:
+                    if strokesHeight < 2 * h:
+                        print(cutIndices[i], "passed condition4")
+                        hp = np.sum(segment[:baselineIndex, :], axis=1)
+                        hp = hp[hp != 0]
+
+                        if stats.mode(hp).mode[0] == mvf:
+                            print(cutIndices[i], "passed condition5")
+                            strokesIndices.append(i)
+
+    # check that last letter is not split
+
+    lastSegment = wordSkeleton[:, cutIndices[0] : cutIndices[1]]
+    if (np.sum(lastSegment[baselineIndex + 1 :, :], axis=1)).sum() > (
+        np.sum(lastSegment[0:baselineIndex, :], axis=1)
+    ).sum():
+
+        cutIndices.pop(1)
+
+    # make seen one letter instead of 3
+
+    # print(len(strokesIndices))
+    if len(strokesIndices) > 2:
+        for i in range(len(strokesIndices) - 2):
+            print(i)
+            if (
+                strokesIndices[i + 2] - strokesIndices[i + 1] == 1
+                and strokesIndices[i + 1] - strokesIndices[i] == 1
+            ):
+                cutIndices.pop(strokesIndices[i + 2])
+                cutIndices.pop(strokesIndices[i + 1])
+
+                i += 2
+    
+    return cutIndices
--- skeleton.py
+++ skeleton.py
@@ -46,137 +46,37 @@ for i in range(len(lines_indices) - 1):
             1,
         )
 
-
-# separating words using indices
-word = binary[
-    lines_indices[0] : lines_indices[1], separators[0][1] : separators[0][2],
-]
-
-# character segmentation for a word
-
-wordSkeleton = skeletonize(word).astype(np.float)
-
-# finding baseline index
-projection = np.sum(wordSkeleton, axis=1)
-baselineIndex = np.argmax(projection)
-
-# finding maximum transition index
-verticalChange = []
-for i in range(baselineIndex):
-    verticalChange.append(len(np.where(wordSkeleton[i, :-1] != wordSkeleton[i, 1:])[0]))
-verticalChange = np.asarray(verticalChange)
-maxChangeIndex = max(np.argmax(verticalChange), baselineIndex - 3)
-
-
-# getting separation region indices
-
-separationIndices = np.where(
-    wordSkeleton[maxChangeIndex, :-1] != wordSkeleton[maxChangeIndex, 1:]
-)[0]
-separationIndices = separationIndices.reshape(-1, 2)
-
-# getting cut indices
-vp = np.sum(wordSkeleton, axis=0)
-mvf = stats.mode(vp[vp != 0]).mode[0]
-vpAbove = np.sum(wordSkeleton[:baselineIndex, :])
-vpBelow = np.sum(wordSkeleton[baselineIndex + 1 :, :])
-cutIndices = []
-cutIndices.append(0)
-for i in range(separationIndices.shape[0] - 1):
-    midRegion = separationIndices[i, 1] + int(
-        (separationIndices[i + 1, 0] - separationIndices[i, 1]) / 2
+words = []
+for i in range(len(lines_indices) - 1):
+    # finding baseline index for the entire line
+    baselineIndex = np.argmax(
+        np.sum(binary[lines_indices[i] : lines_indices[i + 1], :], axis=1)
     )
-    while vp[midRegion] > mvf:
-        midRegion += 1
-
-    # 3ashan lamma el7oroof beyet2esem menha 7etta soghayara keda lwahdaha
-    if np.sum(wordSkeleton[:, cutIndices[-1] : midRegion]) > 3:
-        if midRegion - cutIndices[-1] > 2:
-            cutIndices.append(midRegion)
-
-#
-if np.sum(wordSkeleton[:, cutIndices[-1] : wordSkeleton.shape[1]]) > 3:
-    cutIndices.append(wordSkeleton.shape[1] - 1)
-
-cutIndices = list(dict.fromkeys(cutIndices))
-# strokes detection
-
-# baselineIndex = np.argmax(np.sum(wordSkeleton, axis=1))
-# using line to get line index not word
-# baselineIndex = np.argmax(
-#     np.sum(binary[lines_indices[0] : lines_indices[1], :], axis=1)
-# )
-print(baselineIndex)
-strokesIndices = []
-length = len(cutIndices) - 1
-
-for i in range(length):
-    segment = wordSkeleton[:, cutIndices[i] : cutIndices[i + 1]]
-    sumTopProjection = (np.sum(segment[0:baselineIndex, :], axis=1)).sum()
-    sumBottomProjection = (np.sum(segment[baselineIndex + 1 :, :], axis=1)).sum()
-
-    if sumTopProjection > sumBottomProjection:
-        vp = np.sum(segment[:baselineIndex, :], axis=0)
-        strokesHeight = np.max(vp)
-        print(cutIndices[i], "passed condition1")
-        h = np.sort(np.sum(segment, axis=1))[::-1][0]
-
-        if strokesHeight < h:
-            print(cutIndices[i], "passed condition2 strokes Height=", strokesHeight)
-            hp = np.sum(segment[:baselineIndex, :], axis=1)
-            hp = hp[hp != 0]
-
-            if stats.mode(hp).mode[0] == mvf:
-                print(cutIndices[i], "passed condition3")
-                strokesIndices.append(i)
-        elif len(strokesIndices) >= 2:
-            if i - strokesIndices[-1] == 1 and i - strokesIndices[-2] == 2:
-                if strokesHeight < 2 * h:
-                    print(cutIndices[i], "passed condition4")
-                    hp = np.sum(segment[:baselineIndex, :], axis=1)
-                    hp = hp[hp != 0]
-
-                    if stats.mode(hp).mode[0] == mvf:
-                        print(cutIndices[i], "passed condition5")
-                        strokesIndices.append(i)
-
-# check that last letter is not split
-
-lastSegment = wordSkeleton[:, cutIndices[0] : cutIndices[1]]
-if (np.sum(lastSegment[baselineIndex + 1 :, :], axis=1)).sum() > (
-    np.sum(lastSegment[0:baselineIndex, :], axis=1)
-).sum():
-
-    cutIndices.pop(1)
-
-# make seen one letter instead of 3
-
-# print(len(strokesIndices))
-# if len(strokesIndices) > 2:
-#     for i in range(len(strokesIndices) - 2):
-#         print(i)
-#         if (
-#             strokesIndices[i + 2] - strokesIndices[i + 1] == 1
-#             and strokesIndices[i + 1] - strokesIndices[i] == 1
-#         ):
-#             cutIndices.pop(strokesIndices[i + 2])
-#             cutIndices.pop(strokesIndices[i + 1])
-
-#             i += 2
+    for j in range(len(separators[i])-1, 1, -1):
+        # separating words using indices
+        word = binary[
+            lines_indices[i] : lines_indices[i + 1],
+            separators[i][j - 1] : separators[i][j],
+        ]
+        # character segmentation for a word
+        wordSkeleton = skeletonize(word).astype(np.float)
+        cutIndices = character_segmentation(wordSkeleton, baselineIndex)
+        words.append([wordSkeleton, cutIndices])
+        wordSkeleton[:, cutIndices] = 0.5
+        view = ImageViewer(wordSkeleton)
+        view.show()
 
 
-printWord = word.copy()
-word[:, cutIndices] = 0.5
-wordSkeleton[:, cutIndices] = 0.5
-strokes = []
-for i in range(len(strokesIndices)):
-    strokes.append(cutIndices[strokesIndices[i]])
-wordSkeleton[:, strokes] = 0.3
+# printWord = word.copy()
+# word[:, cutIndices] = 0.5
+# wordSkeleton[:, cutIndices] = 0.5
+# strokes = []
+# for i in range(len(strokesIndices)):
+#     strokes.append(cutIndices[strokesIndices[i]])
+# wordSkeleton[:, strokes] = 0.3
 
 
 # viewing the images
-viewer = CollectionViewer(
-    [img, 1 - corrected, lines_segmented, wordsRects, printWord, word, wordSkeleton]
-)
+viewer = CollectionViewer([img, 1 - corrected, lines_segmented, wordsRects])
 viewer.show()
 
